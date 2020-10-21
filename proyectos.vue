cf58aac780383c743d9ebcb8adda2b32af05209f
Felipe <pife00@gmail.com>
2020-10-20 22:24:05 -0500

    Añadido vuejs

---
+++ proyectos.vue
@@ -0,0 +1,22 @@
+<template>
+        <div class="container">
+        <div class="row">
+            <div class="card bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-header"></div>
+                <div class="card-body">
+                    <a target="_blank" href="">
+                        <img class="img-fluid"
+                            src="" alt="">
+                    </a>
+                    <h5 class="card-title"></h5>
+                    <p class="card-text"></p>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+<script>
+export default {
+    
+}
+</script>
